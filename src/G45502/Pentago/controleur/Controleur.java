9259d239d44ee61541b21cbdb6481d8a82cf8dd7
square0429 <christopher.bourrel0429@gmail.com>
2019-03-30 22:22:03 +0100

    Create logique Game

---
+++ src/G45502/Pentago/controleur/Controleur.java
@@ -0,0 +1,43 @@
+package G45502.Pentago.controleur;
+
+import G45502.Pentago.model.Game;
+import G45502.Pentago.model.State;
+import G45502.Pentago.view.View;
+
+
+/**
+ *
+ * @author G45502
+ */
+public class Controleur {
+    
+    private final Game game;
+    private final View view;
+
+    public Controleur(Game game, View view) {
+        this.game = game;
+        this.view = view;
+    }
+    
+    public void startGame(){
+        while (game.getPlayers().size() < 2) {            
+            game.addPlayer(view.askPlayer());
+        }
+        while (!game.isOver()) {
+            view.viewBoard();
+            game.placePiece(view.askX(), view.askY(), view.askQuadrant());
+            view.askRotate();
+            if (!game.getBoard().isFreePlaceOnBoard()) {
+                game.setState(State.OVER);
+            }
+            if (!game.isOver()) {
+                game.changeCurrentPlayer();
+            }
+        }
+        view.showWinner(game.getWinners());
+    }
+    
+    
+
+   
+}
