c807efc4cdfd9b5fea11bc505b5ae09c256f24d3
G45502 <christopher.bourrel0429@gmail.com>
2019-05-10 18:05:15 +0200

    New End of Game alert

---
+++ src/G45502/Pentago/alert/EndGame.java
@@ -0,0 +1,17 @@
+package G45502.Pentago.alert;
+
+import javafx.scene.control.Alert;
+
+/**
+ *
+ * @author G45502
+ */
+public class EndGame extends Alert {
+
+    public EndGame() {
+        super(AlertType.ERROR);
+        this.setTitle("End of the Game");
+        this.setHeaderText("Ooops, there was an error!");
+        this.setContentText("The game as ended, you can't interact with the board");
+    }
+}
